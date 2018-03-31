26bc51a526d01ed8ceff89244fdefc323c0fe149
Joao Henriques David Dos Reis <jjr.reis@gmail.com>
2018-03-31 14:31:34 +0100

    First commit of running program

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Joao Henriques David Dos Reis on 31/03/2018.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
