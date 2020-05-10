3edc26ee7701e6f41cc0c488e3a29096ecc1b7fd
Joao Reis <joao.reis@leica-geosystems.com>
2020-05-10 12:56:02 +0200

    chapter4

--- CPlusPlusTemplates/Chapter4.h
+++ CPlusPlusTemplates/Chapter4.h
@@ -8,21 +8,23 @@
 /* Non Type Template Parameters
  * */
 
-namespace chapter4 {
+namespace chapter4
+{
 /**
  * Stack with a fixed size of elements
  * @tparam T
  * @tparam MAXSIZE
  */
 template<typename T, int MAXSIZE>
-class Stack {
+class Stack
+{
 private:
     T _elements[MAXSIZE];
     int _numElements;
 public:
-    Stack() = default;
+    Stack();
 
-    void push(const T& elem);
+    void push( const T& elem );
 
     void pop();
 
@@ -30,35 +32,50 @@ public:
 
     bool empty() const
     {
-        return _numElements==0;
+        return _numElements == 0;
     }
 
     bool full() const
     {
-        return _numElements==MAXSIZE;
+        return _numElements == MAXSIZE;
     }
 };
+
 template<typename T, int MAXSIZE>
-void Stack<T, MAXSIZE>::push(const T& elem)
+void Stack<T, MAXSIZE>::push( const T& elem )
 {
-    if(full())
-        throw std::out_of_range("Stack<>::push(): stack is full");
+    if ( full() )
+        throw std::out_of_range( "Stack<>::push(): stack is full" );
     _elements[_numElements] = elem;
     ++_numElements;
 }
+
 template<typename T, int MAXSIZE>
 void Stack<T, MAXSIZE>::pop()
 {
-    if(_numElements <= 0)
-        throw std::out_of_range("Stack<>::push(): stack is empty");
+    if ( _numElements <= 0 )
+        throw std::out_of_range( "Stack<>::push(): stack is empty" );
     --_numElements;
 }
+
 template<typename T, int MAXSIZE>
 T Stack<T, MAXSIZE>::top() const
 {
-    if(_numElements <= 0)
-        throw std::out_of_range("Stack<>::top(): stack is empty");
+    if ( _numElements <= 0 )
+        throw std::out_of_range( "Stack<>::top(): stack is empty" );
     return _elements[_numElements - 1];
+}
+
+template<typename T, int MAXSIZE>
+Stack<T, MAXSIZE>::Stack(): _numElements(0)
+{
+
+}
+
+/// Non Type Function Templates
+template <typename T, int val>
+T AddValue ( const T& x) {
+    return x + val;
 }
 
 }
--- tests/TemplatesTest/chapter4_tests.cpp
+++ tests/TemplatesTest/chapter4_tests.cpp
@@ -4,6 +4,7 @@
 
 #include "gtest/gtest.h"
 #include "../../CPlusPlusTemplates/Chapter4.h"
+#include <algorithm>
 
 using namespace chapter4;
 
@@ -34,4 +35,30 @@ TEST(Chapter4, pushMoreFullStack)
     s.push(3);
     s.push(4);
     ASSERT_THROW(s.push(5), std::out_of_range);
+}
+
+TEST(Chapter4, stringtest)
+{
+    Stack<std::string, 40> stringStack;
+    int N = 40;
+
+    while(N--)
+    {
+        stringStack.push("Hello");
+    }
+
+    ASSERT_TRUE(stringStack.full());
+}
+
+TEST( Chapter4, AddValue)
+{
+    std::vector<int> source {1,2,3,4,5};
+
+    std::transform( source.begin(), source.end(), source.begin(), AddValue < int, 5 > );
+
+    ASSERT_EQ(*source.begin(), 6);
+    ASSERT_EQ(*(source.begin() + 1), 7);
+    ASSERT_EQ(*(source.begin() + 2), 8);
+    ASSERT_EQ(*(source.begin() + 3), 9);
+    ASSERT_EQ(*(source.begin() + 4), 10);
 }
